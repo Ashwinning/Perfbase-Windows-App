ddde1c7562ed8a3aa8492764335c57cf0a1a2fc6
Ashwin Sinha <ash@ds.gg>
2016-07-06 16:58:17 -0700

    Getting correct memory readings! =D

--- Form1.cs
+++ Form1.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Perfbase;
 
 namespace Perfbase_Windows_App
 {
@@ -19,16 +21,54 @@ namespace Perfbase_Windows_App
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            /*
             OpenHardwareMonitor.Hardware.Computer computer = new OpenHardwareMonitor.Hardware.Computer();
             computer.Open();
-            Console.WriteLine(computer.Test());
             computer.CPUEnabled = true;
+            computer.FanControllerEnabled = true;
+            computer.GPUEnabled = true;
+            computer.HDDEnabled = true;
+            computer.MainboardEnabled = true;
+            computer.RAMEnabled = true;
             List<Perfbase.HardwareStats> stats = computer.GetPerfReport();
-            Console.WriteLine(computer.Test());
-            Console.WriteLine("Length = " + stats.Count);
             string json = Newtonsoft.Json.JsonConvert.SerializeObject(stats);
-            //Console.Write(json);
+            Console.WriteLine(" --- ");
+            Console.Write(json);
+            */
+
+            test();
+
         }
+
+        void test()
+        {
+            // Get all processes running on the local computer.
+            List<PerfProcess> processes = new List<PerfProcess>();
+            Process[] localAll = Process.GetProcesses();
+            foreach (Process process in localAll)
+            {
+                PerformanceCounter theCPUCounter = new PerformanceCounter("Process", "% Processor Time", process.ProcessName);
+                //PerformanceCounter theMemCounter = new PerformanceCounter("Process", "Working Set", process.ProcessName);
+
+                PerfProcess newProcess = new PerfProcess();
+                newProcess.name = process.ProcessName;
+                //newProcess.cpuUsage = theCPUCounter.NextValue();
+                newProcess.memUsage = PerfProcessMemoryUsage(process);
+                processes.Add(newProcess);
+            }
+            processes.Sort((a, b) => a.name.CompareTo(b.name));
+
+            foreach (PerfProcess proc in processes)
+            {
+                Console.WriteLine(proc.name + " | " + proc.cpuUsage + " | " + proc.memUsage);
+            }
+        }
+
+        private float PerfProcessMemoryUsage(Process process)
+        {
+            var counter = new PerformanceCounter("Process", "Working Set - Private", process.ProcessName);
+            return counter.NextValue() / 1024 / 1024;
+        }
+
     }
 }
---
+++ Processes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perfbase
+{
+    class Processes
+    {
+    }
+
+    class PerfProcess
+    {
+        public string name;
+        public float cpuUsage;    //bytes
+        public float memUsage;    //bytes
+    }
+}
