7cac6a7c6c6a86b47a47ef5ec6136d4d17889a05
Ashwin Sinha <ash@ds.gg>
2016-07-06 12:18:06 -0700

    Getting sensor types

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,19 @@ namespace Perfbase_Windows_App
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            OpenHardwareMonitor.Hardware.Computer computer = new OpenHardwareMonitor.Hardware.Computer();
+            computer.Open();
+            Console.WriteLine(computer.Test());
+            computer.CPUEnabled = true;
+            List<Perfbase.HardwareStats> stats = computer.GetPerfReport();
+            Console.WriteLine(computer.Test());
+            Console.WriteLine("Length = " + stats.Count);
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(stats);
+            //Console.Write(json);
+        }
     }
 }
